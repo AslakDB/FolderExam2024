3b231b3454440101962ca69c15b8de3611605a0e
AslakDB <Askelakken@gmail.com>
2024-11-21 09:19:04 +0100

    working with barycentric cooardiantes
    also added chrono library to check time

--- FolderExam2024/Core/render.h
+++ FolderExam2024/Core/render.h
@@ -8,6 +8,7 @@
 #include "Sphere.h"
 #include "Collision.h"
 #include "PointCloud.h"
+#include <chrono>
 
 PointCloud pointCloud;
 Sphere sphere;
@@ -26,8 +27,9 @@ void ProsessInput(GLFWwindow *window, float deltaTime, model& sphere);
 float calculateNormal(glm::vec3& vektor1, glm::vec3& vektor2);
 bool calculateBarycentric(Vertex& P, Vertex& R, Vertex& Q, glm::vec3& PlayerPos);
 
-struct Render {
-bool inside;
+struct Render
+{
+    bool inside;
     bool isMovingForward;
     
     Render() = default;
@@ -52,13 +54,17 @@ bool inside;
 
         glm::mat4 trans = glm::mat4(1.0f);
         glm::mat4 projection;
-        
-        pointCloud.CreatePlane( PCloud, "Trondheim_punkt_sky_comp6.txt");
+
+        std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+        pointCloud.CreatePlane( PCloud, "Trondheim_punkt_sky_comp3.txt");
+        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+        std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::seconds>(end - begin).count() << "[seconds]" << std::endl;
+
         
         sphere.CreateSphere(SphereModel0);
-      sphere.CreateSphere(SphereModel1);
-      sphere.CreateSphere(SphereModel2);
-      sphere.CreateSphere(SphereModel3);
+        sphere.CreateSphere(SphereModel1);
+        sphere.CreateSphere(SphereModel2);
+        sphere.CreateSphere(SphereModel3);
         sphere.CreateSphere(SphereModel4);
         
         
@@ -81,7 +87,7 @@ bool inside;
         XWallP.PlayerScale = glm::vec3(0.1f,1.f,1.f);
 
         SphereModel0.PlayerPos = glm::vec3(-1.f,1.f,-1.f);
-        SphereModel1.PlayerPos = glm::vec3(-2.f,-0.1f,0.2f);
+        SphereModel1.PlayerPos = glm::vec3(-6.f,-0.1f,5.2f);
         SphereModel2.PlayerPos = glm::vec3(0.f,0.1f,-1.f);
         SphereModel3.PlayerPos = glm::vec3(2.f,-0.1f,0.f);
         SphereModel4.PlayerPos = glm::vec3(1.f,0.1f,-1.f);
@@ -89,161 +95,170 @@ bool inside;
 
         
         while (!glfwWindowShouldClose(window))
-            {
+        {
 
             coll.SphereSphereCollision(sphere_models);
-           coll.SphereBoxCollision(sphere_models,models);
+            coll.SphereBoxCollision(sphere_models,models);
 
             
-           sphere.Move(SphereModel0, deltaTime, SphereModel0.Velocity);
-           sphere.Move(SphereModel1, deltaTime, SphereModel1.Velocity);
-           sphere.Move(SphereModel2, deltaTime, SphereModel2.Velocity);
-           sphere.Move(SphereModel3, deltaTime, SphereModel3.Velocity);
+            sphere.Move(SphereModel0, deltaTime, SphereModel0.Velocity);
+            sphere.Move(SphereModel1, deltaTime, SphereModel1.Velocity);
+            sphere.Move(SphereModel2, deltaTime, SphereModel2.Velocity);
+            sphere.Move(SphereModel3, deltaTime, SphereModel3.Velocity);
             sphere.Move(SphereModel4, deltaTime, SphereModel4.Velocity);
-            
-            float currentFrame = glfwGetTime();
-            deltaTime = currentFrame - lastFrame;
-            lastFrame = currentFrame;
-            ProsessInput(window, deltaTime, ZWallN);
+
+
+            /*for (auto Spheres : sphere_models)
+            {
+                for (auto element : PCloud.indices)
+                {
+                    calculateBarycentric( PCloud.vertices[element.A],PCloud.vertices[element.B],PCloud.vertices[element.C], Spheres->PlayerPos);
+                }
+            }*/
+                float currentFrame = glfwGetTime();
+                deltaTime = currentFrame - lastFrame;
+                lastFrame = currentFrame;
+                ProsessInput(window, deltaTime, ZWallN);
             
 
-            projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+                projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
-            camera.tick(shaderProgram);
+                camera.tick(shaderProgram);
 
        
-            glClearColor(0.5f, 0.99f, 0.5f, 1.0f);
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+                glClearColor(0.5f, 0.99f, 0.5f, 1.0f);
+                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-            int viewLoc = glGetUniformLocation(shaderProgram, "viewPos");
-            glUniform3fv(viewLoc, 1, glm::value_ptr(camera.cameraPos));
+                int viewLoc = glGetUniformLocation(shaderProgram, "viewPos");
+                glUniform3fv(viewLoc, 1, glm::value_ptr(camera.cameraPos));
 
-            int LightLoc = glGetUniformLocation(shaderProgram, "lightPos");
-            glUniform3fv(LightLoc, 1, glm::value_ptr(glm::vec3(0,10,0)));
+                int LightLoc = glGetUniformLocation(shaderProgram, "lightPos");
+                glUniform3fv(LightLoc, 1, glm::value_ptr(glm::vec3(0,10,0)));
 
-            glLineWidth(3);
+                glLineWidth(3);
 
-           // glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
+                // glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
             
-            for (model* element : sphere_models)
-            {
-                element->CalculateMatrix();
-                element->CalculateBoundingBox();
-                element->DrawMesh(shaderProgram);
+                for (model* element : sphere_models)
+                {
+                    element->CalculateMatrix();
+                    element->CalculateBoundingBox();
+                    element->DrawMesh(shaderProgram);
 
-            }
-            for (model* element : models)
-            {
-                element->CalculateMatrix();
-                element->CalculateBoundingBox();
-                //glPointSize(2.f);
-                element->DrawMesh(shaderProgram);
-            }
+                }
+                for (model* element : models)
+                {
+                    element->CalculateMatrix();
+                    element->CalculateBoundingBox();
+                    //glPointSize(2.f);
+                    element->DrawMesh(shaderProgram);
+                }
 
-            // bsplineModel2.CalculateMatrix();
-            // bsplineModel2.DrawMesh(shaderProgram);
+                // bsplineModel2.CalculateMatrix();
+                // bsplineModel2.DrawMesh(shaderProgram);
             
-            glfwSwapBuffers(window);
-            glfwPollEvents();
+                glfwSwapBuffers(window);
+                glfwPollEvents();
             }
-    }
-};
+        }
+    };
 
-void mouse_callback(GLFWwindow* window, double xpos, double ypos)
-{
-    if (firstMouse)
+    void mouse_callback(GLFWwindow* window, double xpos, double ypos)
     {
+        if (firstMouse)
+        {
+            lastX = xpos;
+            lastY = ypos;
+            firstMouse = false;
+        }
+
+        float xoffset = xpos - lastX;
+        float yoffset = lastY - ypos;
         lastX = xpos;
         lastY = ypos;
-        firstMouse = false;
+
+        float sensitivity = 0.1f;
+        xoffset *= sensitivity;
+        yoffset *= sensitivity;
+
+        camera.yaw   += xoffset;
+        camera.pitch += yoffset;
+
+        if(camera.pitch > 89.0f)
+            camera.pitch = 89.0f;
+        if(camera.pitch < -89.0f)
+            camera.pitch = -89.0f;
+
+        glm::vec3 direction;
+        direction.x = cos(glm::radians(camera.yaw)) * cos(glm::radians(camera.pitch));
+        direction.y = sin(glm::radians(camera.pitch));
+        direction.z = sin(glm::radians(camera.yaw)) * cos(glm::radians(camera.pitch));
+        camera.cameraFront = glm::normalize(direction);
     }
 
-    float xoffset = xpos - lastX;
-    float yoffset = lastY - ypos;
-    lastX = xpos;
-    lastY = ypos;
 
-    float sensitivity = 0.1f;
-    xoffset *= sensitivity;
-    yoffset *= sensitivity;
+    void ProsessInput(GLFWwindow *window, float deltaTime, model& sphere) {
 
-    camera.yaw   += xoffset;
-    camera.pitch += yoffset;
+        glm::vec3 cameraFrontXZ = glm::normalize(glm::vec3(camera.cameraFront.x, 0.0f, camera.cameraFront.z));
 
-    if(camera.pitch > 89.0f)
-        camera.pitch = 89.0f;
-    if(camera.pitch < -89.0f)
-        camera.pitch = -89.0f;
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(window, true);
 
-    glm::vec3 direction;
-    direction.x = cos(glm::radians(camera.yaw)) * cos(glm::radians(camera.pitch));
-    direction.y = sin(glm::radians(camera.pitch));
-    direction.z = sin(glm::radians(camera.yaw)) * cos(glm::radians(camera.pitch));
-    camera.cameraFront = glm::normalize(direction);
-}
+        float cameraSpeed = 2.5f * deltaTime ;
+        if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+            camera.cameraPos += cameraSpeed * cameraFrontXZ;
+            /*Player.PlayerPos +=  cameraSpeed * cameraFrontXZ;*/}
+        if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        { camera.cameraPos -= cameraSpeed * cameraFrontXZ;
+            /*Player.PlayerPos -=  cameraSpeed * cameraFrontXZ;*/}
+        if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS){
+            camera.cameraPos -= glm::normalize(glm::cross(camera.cameraFront, camera.cameraUp)) * cameraSpeed;
+            /*Player.PlayerPos -=  glm::normalize(glm::cross(camera.cameraFront, camera.cameraUp)) * cameraSpeed;*/}
+        if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS){
+            camera.cameraPos += glm::normalize(glm::cross(camera.cameraFront, camera.cameraUp)) * cameraSpeed;
+            /*Player.PlayerPos +=  glm::normalize(glm::cross(camera.cameraFront, camera.cameraUp)) * cameraSpeed;*/}
+        if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+            camera.cameraPos += cameraSpeed * camera.cameraUp; // Move camera up
+        if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+            camera.cameraPos -= cameraSpeed * camera.cameraUp;
 
-
-void ProsessInput(GLFWwindow *window, float deltaTime, model& sphere) {
-
-    glm::vec3 cameraFrontXZ = glm::normalize(glm::vec3(camera.cameraFront.x, 0.0f, camera.cameraFront.z));
-
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-
-    float cameraSpeed = 2.5f * deltaTime ;
-    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
-        camera.cameraPos += cameraSpeed * cameraFrontXZ;
-        /*Player.PlayerPos +=  cameraSpeed * cameraFrontXZ;*/}
-    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-       { camera.cameraPos -= cameraSpeed * cameraFrontXZ;
-        /*Player.PlayerPos -=  cameraSpeed * cameraFrontXZ;*/}
-    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS){
-        camera.cameraPos -= glm::normalize(glm::cross(camera.cameraFront, camera.cameraUp)) * cameraSpeed;
-    /*Player.PlayerPos -=  glm::normalize(glm::cross(camera.cameraFront, camera.cameraUp)) * cameraSpeed;*/}
-    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS){
-        camera.cameraPos += glm::normalize(glm::cross(camera.cameraFront, camera.cameraUp)) * cameraSpeed;
-    /*Player.PlayerPos +=  glm::normalize(glm::cross(camera.cameraFront, camera.cameraUp)) * cameraSpeed;*/}
-    if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
-        camera.cameraPos += cameraSpeed * camera.cameraUp; // Move camera up
-    if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
-        camera.cameraPos -= cameraSpeed * camera.cameraUp;
-
-    if(glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS)
-    {
-        std::cout<<"move forward"<<std::endl;
-        sphere.Velocity = glm::vec3(8.f,0.f,0.f);
-}
+        if(glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS)
+        {
+            std::cout<<"move forward"<<std::endl;
+            sphere.Velocity = glm::vec3(8.f,0.f,0.f);
+        }
     
-}
-
-
-float calculateNormal(glm::vec3& vektor1, glm::vec3& vektor2) {
-    return vektor1[0]* vektor2[2]- vektor2[0]*vektor1[2];
-}
-
-bool calculateBarycentric(Vertex& P, Vertex& R, Vertex& Q, glm::vec3& PlayerPos) {
-    glm::vec3 x1 = Q.XYZ - P.XYZ;
-    glm::vec3 x2 = R.XYZ - P.XYZ;
-    float Areal = calculateNormal(x1,x2);
-
-
-    glm::vec3 u1 = Q.XYZ- PlayerPos;
-    glm::vec3 u2 = R.XYZ- PlayerPos;
-
-    float U = calculateNormal(u1,u2) /Areal;
-
-    glm::vec3 v1 = R.XYZ - PlayerPos;
-    glm::vec3 v2 = P.XYZ - PlayerPos;
-
-    float V = calculateNormal(v1,v2)  / Areal;
-
-    float W = 1 - U - V;
-
-    if (U >=0 && V >= 0 && W >=0)
-    {
-        float height=U* P.XYZ.y+ V * Q.XYZ.y + W * R.XYZ.y;
-        PlayerPos.y = height + 0.5;
-        return true;
     }
-    return false;
-}
+
+
+    float calculateNormal(glm::vec3& vektor1, glm::vec3& vektor2) {
+        return vektor1[0]* vektor2[2]- vektor2[0]*vektor1[2];
+    }
+
+    bool calculateBarycentric(Vertex& P, Vertex& R, Vertex& Q, glm::vec3& PlayerPos) {
+        glm::vec3 x1 = Q.XYZ - P.XYZ;
+        glm::vec3 x2 = R.XYZ - P.XYZ;
+        float Areal = calculateNormal(x1,x2);
+
+
+        glm::vec3 u1 = Q.XYZ- PlayerPos;
+        glm::vec3 u2 = R.XYZ- PlayerPos;
+
+        float U = calculateNormal(u1,u2) /Areal;
+
+        glm::vec3 v1 = R.XYZ - PlayerPos;
+        glm::vec3 v2 = P.XYZ - PlayerPos;
+
+        float V = calculateNormal(v1,v2)  / Areal;
+
+        float W = 1 - U - V;
+
+        if (U >=0 && V >= 0 && W >=0)
+        {
+            float height=U* P.XYZ.y+ V * Q.XYZ.y + W * R.XYZ.y;
+            PlayerPos.y = height + 0.5;
+            return true;
+        }
+        return false;
+    }
+
